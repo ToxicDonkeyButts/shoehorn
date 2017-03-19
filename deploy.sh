8f2df6115d8a35ca9dd0614e85d2737ad8b4d3a5
Lasse Borly <lasseborly@gmail.com>
2017-03-19 04:33:41 +0100

    decode

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 echo "Hello world"
 
 eval $(ssh-agent -s)
-ssh-add <(echo "$SSH_PRIVATE_KEY")
+ssh-add <(echo "$SSH_PRIVATE_KEY" | base64 --decode)
 
 ssh $SSH_USER@$SERVER_IP echo "hello from the server"
