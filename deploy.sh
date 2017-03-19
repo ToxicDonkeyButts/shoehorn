32b2e930fc0db208a5d7f5b6b2ce22e152863532
Lasse Borly <lasseborly@gmail.com>
2017-03-19 04:49:45 +0100

    deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "Hello world"
-
 eval $(ssh-agent -s)
-ssh-add <(echo "$SSH_PRIVATE_KEY" | base64 --decode)
-
+echo "$SSH_PRIVATE_KEY" | base64 --decode > key
+ssh-add key
 ssh $SSH_USER@$SERVER_IP echo "hello from the server"
