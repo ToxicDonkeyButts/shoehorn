0edd3a5b999ac580dcf8cb6c7386580044c488d4
Lasse Borly <lasseborly@gmail.com>
2017-03-19 04:55:23 +0100

    strict

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 eval $(ssh-agent -s)
+echo "StrictHostKeyChecking no" >> .ssh/config
 echo "$SSH_PRIVATE_KEY" | base64 --decode > key
+chmod 600 key
 ssh-add key
 ssh $SSH_USER@$SERVER_IP echo "hello from the server"
