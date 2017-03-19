3d9ce7e4949f05f291c89b6a05a68217146ddaa4
Lasse Borly <lasseborly@gmail.com>
2017-03-19 05:09:18 +0100

    strict no

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ echo "StrictHostKeyChecking no" >> .ssh/config
 echo "$SSH_PRIVATE_KEY" | base64 --decode > key
 chmod 600 key
 ssh-add key
-ssh $SSH_USER@$SERVER_IP echo "hello from the server"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $SSH_USER@$SERVER_IP echo "hello from the server"
