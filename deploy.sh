c7d99b954772e986e027aca84a31494e5191e7c7
Lasse Borly <lasseborly@gmail.com>
2017-03-19 05:19:13 +0100

    sync

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 eval $(ssh-agent -s)
-mkdir -p .ssh/
-echo "StrictHostKeyChecking no" >> .ssh/config
 echo "$SSH_PRIVATE_KEY" | base64 --decode > key
 chmod 600 key
 ssh-add key
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $SSH_USER@$SERVER_IP echo "hello from the server"
+rsync -a -e "ssh -p $SSH_PORT -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $TRAVIS_BUILD_DIR/ $SSH_USER@$SERVER_IP:$DEPLOY_PATH
