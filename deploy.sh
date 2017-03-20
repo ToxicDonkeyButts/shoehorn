efba8c50876547310562b4335881ad1461b646ec
Lasse Borly <lasseborly@gmail.com>
2017-03-20 02:09:24 +0100

    times

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ echo "$SSH_PRIVATE_KEY" | base64 --decode > key
 chmod 600 key
 ssh-add key
 rm key
-rsync -a -e "ssh -p $SSH_PORT -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $TRAVIS_BUILD_DIR/ $SSH_USER@$SERVER_IP:$DEPLOY_PATH
+rsync -a -e -O "ssh -p $SSH_PORT -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $TRAVIS_BUILD_DIR/ $SSH_USER@$SERVER_IP:$DEPLOY_PATH
